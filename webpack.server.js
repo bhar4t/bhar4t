02e2b8000de3ecd6936b41811d4583eb7fdd30bc
bhar4t <seizedbharat@gmail.com>
2020-05-02 21:42:26 +0530

    removed pallete.pdf

--- webpack.server.js
+++ webpack.server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./server/index.js",
 
   target: "node",
 
