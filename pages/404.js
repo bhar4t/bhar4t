066c571e5fac9d75b201e2c801b84c94221f550f
bhar4t <seizedbharat@gmail.com>
2021-03-07 13:36:58 +0530

    error page design.

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-}
+    return <>
+        <div style={{ padding: 20, userSelect: 'none', height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <img style={{ height: '50%', width: 'auto', objectFit: 'contain' }} src="./images/404.gif" alt="404.gif"></img>
+            <h2 style={{ textAlign: 'center' }}>404 | This Page Could Not Be Found.</h2>
+            <Link href="/"><a>Back to Home</a></Link>
+      </div>
+    </>
+  }
