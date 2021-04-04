c7b1bfcb423ca5a8600659729f539168720da42c
bhar4t <seizedbharat@gmail.com>
2021-04-04 16:07:50 +0530

    minor

--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Bootstrap */}
-        {/* <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-          crossOrigin="anonymous"
-          rel="preload"
-          as="style"
-          onLoad="this.onload=null;this.rel='stylesheet'"
-        /> */}
         {/* Local fonts */}
         <link
             rel="preload"
