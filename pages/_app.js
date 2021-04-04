2403a3aeeaa6b95680237f0923c0e6e83b2350a7
bhar4t <seizedbharat@gmail.com>
2021-04-04 15:47:48 +0530

    check local import of bootrap-grid css.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import "../styles/global.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         {/* Bootstrap */}
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
           rel="preload"
           as="style"
           onLoad="this.onload=null;this.rel='stylesheet'"
-        />
+        /> */}
         {/* Local fonts */}
         <link
             rel="preload"
