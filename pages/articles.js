9e5cd10a6e783b5a6168a07fae5f8e57dc845595
bhar4t <seizedbharat@gmail.com>
2021-04-05 17:20:26 +0530

    mobile view for articles  list.

--- pages/articles.js
+++ pages/articles.js
@@ -15,17 +15,17 @@ export default function Home({ allPostsData }) {
       <section>
         {allPostsData.map(({ id, date, title, cover, author, description, keywords }, i) => (
           <Link href="/articles/[id]" as={`/articles/${id}`}>
-          <div key={id} style={{ display: 'flex', width: '100%', margin: '10px 0px', }}>
-            <div style={{ flex: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <div style={{ height: 'auto', width: '26vh', objectFit: 'contain'  }}>
+          <div key={id} style={{ display: 'flex', flexWrap: 'wrap', lineHeight: 1.5, border: '1px solid #f5f0c2', borderRadius: 4, width: '100%', margin: '10px 0px', }}>
+            <div style={{ flex: 2, display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
+              <div style={{ height: 'auto', width: '30vh',padding: 2, objectFit: 'contain'  }}>
                 <img height="100%" width="100%" src={`/img/${cover}`} alt={title}></img>
               </div>
             </div>
             <div style={{ flex: 6, padding: 6 }}>
-              <a>{title}</a>
-              <div style={{ padding: 3, fontSize: 14 }}>{description}</div>
-              <div style={{ fontSize: 11, padding: 2, margin: 2 }}>{keywords.split(', ').map(e => <span style={{ backgroundColor: '#d3bf00', borderRadius: 5, marginLeft: 4, padding: '3px 5px' }}>{e}</span>)}</div>
-              <div style={{ padding: 3, color: '#d3bf00', fontSize: 14 }}><Link href="/bhar4t" as={`/bhar4t`}>{author + ", "}</Link>{<Date dateString={date} />}</div>
+              <a style={{ padding: '6px 2px' }}>{title}</a>
+              <div style={{ padding: '6px 4px', fontSize: 14 }}>{description}</div>
+              <div style={{ fontSize: 11, padding: '6px 4px', margin: 2 }}>{keywords.split(', ').map(e => <span style={{ backgroundColor: '#d3bf00', borderRadius: 5, marginLeft: 4, padding: '3px 5px' }}>{e}</span>)}</div>
+              <div style={{ padding: '6px 3px', color: '#d3bf00', fontSize: 14 }}><Link href="/bhar4t" as={`/bhar4t`}>{author + ", "}</Link>{<Date dateString={date} />}</div>
             </div>
           </div>
           </Link>
