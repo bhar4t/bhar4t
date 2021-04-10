5002d30b16e4dce2403fc8e231ec65ff0ebfabb8
bhar4t <seizedbharat@gmail.com>
2021-04-10 18:58:16 +0530

    external css need to optimize, articles

--- pages/articles.js
+++ pages/articles.js
@@ -6,26 +6,27 @@ import Link from "next/link";
 import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
+  const {
+    preTitle, articleList, card, imgContainer, img, textContainer,  articleTitle,  articleDesc,  articleKeys,  articleKey, articleAuthor
+  } = utilStyles;
   return (
     <Layout home>
-      <span className={utilStyles.preTitle}>Articles</span>
-      <br /><br />
-      <br /><br />
-      <br /><br />
-      <section>
+      <span className={preTitle}>Articles</span>
+      <section className={articleList}>
         {allPostsData.map(({ id, date, title, cover, author, description, keywords }, i) => (
           <Link href="/articles/[id]" as={`/articles/${id}`}>
-          <div key={id} style={{ display: 'flex', flexWrap: 'wrap', lineHeight: 1.5, border: '1px solid #f5f0c2', borderRadius: 4, width: '100%', margin: '10px 0px', }}>
-            <div style={{ flex: 2, display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
-              <div style={{ height: 'auto', width: '30vh',padding: 2, objectFit: 'contain'  }}>
+          <div key={id} className={card}>
+            <div className={imgContainer}>
+              <div className={img}>
                 <img loading="lazy" height="100%" width="100%" src={`/img/${cover}`} alt={title}></img>
               </div>
             </div>
-            <div style={{ flex: 6, padding: 6 }}>
-              <a style={{ padding: '6px 2px' }}>{title}</a>
-              <div style={{ padding: '6px 4px', fontSize: 14 }}>{description}</div>
-              <div style={{ fontSize: 11, padding: '6px 4px', margin: 2 }}>{keywords.split(', ').map(e => <span style={{ backgroundColor: '#d3bf00', borderRadius: 5, marginLeft: 4, padding: '3px 5px' }}>{e}</span>)}</div>
-              <div style={{ padding: '6px 3px', color: '#d3bf00', fontSize: 14 }}><Link href="/bhar4t" as={`/bhar4t`}>{author + ", "}</Link>{<Date dateString={date} />}</div>
+            <div className={textContainer}>
+              <a className={articleTitle}>{title}</a>
+              <div className={articleDesc}>{description}</div>
+              <div className={articleKeys}>
+                {keywords.split(', ').map(e => <span className={articleKey}>{e}</span>)}</div>
+              <div className={articleAuthor}><Link href="/bhar4t" as={`/bhar4t`}>{author + ", "}</Link>{<Date dateString={date} />}</div>
             </div>
           </div>
           </Link>
