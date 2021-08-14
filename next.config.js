b35b41302676403aa6f60a94b2368823ba9eb6f9
BHAR4T <bhar4t@outlook.com>
2021-08-14 22:20:22 +0530

    `webpack5` has moved 2 top lvl in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
   },
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   env: {
     PRE_TITLE: 'Code with',
     TITLE: 'Bharat Sahu',
